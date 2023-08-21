bba2ec83c56b3479fff4f9e4d1b1faf1cfb80346
yehuda shani <yehudashani88@gmail.com>
2023-08-21 14:36:26 +0300

    added incomplete search function

--- search.py
+++ search.py
@@ -38,6 +38,22 @@ def autocomplete_no_mistakes(sentence: str, sentence_trie: SentenceTrie, words_t
         terminal_nodes += sentence_trie.find_all_terminals(reference)
     return terminal_nodes
 
+def autocomplete_with_incomplete_last_word(incomplete_sentence: str, sentence_trie: SentenceTrie, words_trie: Trie) -> List[SentenceNode]:
+    """
+    Receives a sentence and returns a list of all the possible completions
+    :param sentence: str
+    :param sentence_trie: SentenceTrie object
+    :param words_trie: Trie object
+    :return: a list of all the possible completions
+    """
+    words = incomplete_sentence.split()
+    possible_last_words = words_trie.query(words[-1])
+    possible_last_words = [word[0] for word in possible_last_words]
+    sentence_nodes = []
+    for word in possible_last_words:
+        sentence_nodes += autocomplete_no_mistakes(' '.join(words[:-1]) + ' ' + word, sentence_trie, words_trie)
+    return sentence_nodes
+
 def main():
     sentence_trie = SentenceTrie()
     sentence_trie.add_sentence('hello world')
@@ -45,7 +61,10 @@ def main():
     words_trie = Trie()
     words_trie.insert_data(sentence_trie)
     nodes = autocomplete_no_mistakes('hello', sentence_trie, words_trie)
+    incomplete_nodes = autocomplete_with_incomplete_last_word('hello wo', sentence_trie, words_trie)
     for node in nodes:
+        print(complete_sentence(node))
+    for node in incomplete_nodes:
         print(complete_sentence(node))
 
 if __name__ == '__main__':
