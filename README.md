f51c7d0a0c68968b75dab651b56b7c084b5dcb01
omer <omerzki@gmail.com>
2023-08-22 11:52:01 +0300

    read all data

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# google-google-group-1
+# group-1
+
+# Project Decription:
+
+Aoutocomplete search typing in DataBase of txt files.
+
+
+## Algorithms & Structures
+
+The structure based on two trie-trees:
+	 1. Sentences trie-tree (we'll call it STT from now).
+	 2. words trie-tree (we'll call it WTT from now).
+
+The STT holds all the lines in the data-base.
+The WTT holds all the words in the data-base.
+
+
--- getData.py
+++ getData.py
@@ -1,8 +1,10 @@
 # take all txt files from dir
 
 import Sentence_trie
+import words_trie
 import logging
 import glob
+
 ROOT_DIR = r'C:\BootCamp2023\google_project\rpoject\Texts'
 
 sentenceTrie = Sentence_trie.SentenceTrie()
@@ -12,7 +14,7 @@ logging.basicConfig(level=logging.INFO)
 
 def get_txt_files() -> list[str]:
     # Returns a list of names in list files.
-    return glob.glob(ROOT_DIR + '**/*.txt', recursive=True)
+    return glob.glob(ROOT_DIR + '/**/*.txt', recursive=True)
 
 
 def read_lines(path: str):
@@ -21,15 +23,37 @@ def read_lines(path: str):
         for line in file:
             count += 1
             sentenceTrie.add_sentence(line.strip())
-            print(line.strip())
-            logging.info(f"line{count}: {line}")
+            #   print(count, ": ", line.strip())
+            # logging.info(f"line{count}: {line}")
 
 
 def main():
-    files = get_txt_files()
-    for file in files:
-        print(file)
-        read_lines(file)
+    files = None
+    try:
+        files = get_txt_files()
+        print(files[2])
+    except Exception as ex:
+        print(ex)
+
+    try:
+        # print(files[2])
+        #
+        # read_lines(files[2])
+
+
+        for file in files:
+            print(file)
+            read_lines(file)
+    except Exception as ex:
+        print(ex)
+    print("Sentences Done")
+
+    try:
+        wordsTrie = words_trie.Trie()
+        wordsTrie.insert_data(sentenceTrie)
+    except Exception as ex:
+        print(ex)
+    print("Words Done")
 
 
 if __name__ == "__main__":
